8d822ec7fec56389268b3ef74ebc7d8ec0a79756
Shyam kumar <83579268+shyam242@users.noreply.github.com>
2026-06-02 16:11:59 +0530

    Update authControllers.js

--- backend/server/controllers/authControllers.js
+++ backend/server/controllers/authControllers.js
@@ -29,7 +29,9 @@ const getTransporter = () => {
     }
     
     transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
