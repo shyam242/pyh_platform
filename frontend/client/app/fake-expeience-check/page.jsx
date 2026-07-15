f1e45990e21d6e445aed48a55f884ca2eb1525e2
Shyam kumar <83579268+shyam242@users.noreply.github.com>
2026-07-15 16:39:39 +0530

    Create page.jsx

---
+++ frontend/client/app/fake-expeience-check/page.jsx
@@ -0,0 +1,6 @@
+"use client";
+import FakeExperienceCheck from "@/components/FakeExperienceCheck";
+
+export default function FakeExperienceCheckPage() {
+  return <FakeExperienceCheck apiPrefix="/api/recruiter" backHref="/recruiter" />;
+}
